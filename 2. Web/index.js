fe0c8f9e3f116f11e9db09a872bbae7d1dc12cad
hwajeong <honeylee999@icloud.com>
2021-05-08 10:27:48 +0900

    2021-05-08 JS practice

---
+++ 2. Web/index.js
@@ -0,0 +1,18 @@
+console.log('HELLO HONEY')
+
+function greeting(name){
+  console.log('안녕하세요!' + name);
+}
+
+greeting(honey);
+
+let x = 10;
+let y = 20;
+
+function myFunction(a, b) {
+  return a + b;
+}
+
+let myAnswer = myFunction('x', 'y');
+
+console.log(myAnswer);
